9b59826041a54d9cae730153aa7de13409d2cad1
Oliver Lindemann <lindemann09@googlemail.com>
2024-12-06 15:55:49 +0100

    test trigger

--- befordata/__init__.py
+++ befordata/__init__.py
@@ -1,7 +1,7 @@
 """BeFoR-Data structure for handling behavioural force data"""
 
 __author__ = "Oliver Lindemann"
-__version__ = "0.2.4-dev1"
+__version__ = "0.2.4"
 
 from ._epochs import BeForEpochs
 from ._record import BeForRecord
