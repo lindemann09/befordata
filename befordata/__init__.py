1741de0f1be9a6b0416f4994dd4a0d49d2a8568d
Oliver Lindemann <lindemann09@users.noreply.github.com>
2025-07-27 18:21:00 +0200

    bump version

--- befordata/__init__.py
+++ befordata/__init__.py
@@ -1,7 +1,7 @@
 """BeFoR-Data structure for handling behavioural force data"""
 
 __author__ = "Oliver Lindemann"
-__version__ = "0.3.0"
+__version__ = "0.3.1-dev0"
 
 from ._epochs import BeForEpochs
 from ._record import BeForRecord
